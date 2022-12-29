0fdc6ee9d08a447d7a017c58f8537ae820f435ff
Tornado7777 <Tornadod@mail.ru>
2022-12-30 00:07:42 +0300

    e

--- SertGenerate/Program.cs
+++ SertGenerate/Program.cs
@@ -10,6 +10,69 @@ namespace SertGenerate
     {
         static void Main(string[] args)
         {
+            while(true)
+            {
+                //Console.Clear();
+                Console.WriteLine("~~ Центр генерации сертификатов ~~\n");
+                Console.WriteLine("1. Создать коренвой сертификат");
+                Console.WriteLine("2. Создать сертификат");
+                Console.WriteLine("Выберите подпрограмму (0 - завершение работы приложения):");
+                if (int.TryParse(Console.ReadLine(), out int value)) 
+                { 
+                    switch(value)
+                    {
+                        case 0:
+                            Console.WriteLine("завершение работы приложения.");
+                            Console.ReadKey();
+                            return;
+                        case 1:
+
+                            CertificateConfiguration certificateConfiguration = new CertificateConfiguration
+                            {
+                                CertName = "Locky Co CA",
+                                Email = "test@gmail.com",
+                                OutFolder = @"D:\Users\certificate\",
+                                Password= "12345678",
+                                CertDuration = 30
+                            };
+                            CertificateGenerationProvider certificateGenerationProvider= new CertificateGenerationProvider();
+                            certificateGenerationProvider.GenerateRootCertificate(certificateConfiguration);
+                            Console.WriteLine("Корневой сертификат успешно сгенерирован!");
+                            break; 
+                        case 2:
+                            int counter = 0;
+                            CertificateExplorerProvider certificateExplorerProvider = new CertificateExplorerProvider(true);
+                            certificateExplorerProvider.LoadCertificates();
+                            foreach (var certificate in certificateExplorerProvider.Certificates)
+                            {
+                                Console.WriteLine($"{counter++} >>> {certificate}");
+                            }
+                            Console.WriteLine("Укажите номер корневого сертификата");
+
+                            if (int.TryParse(Console.ReadLine(), out int certNumber))
+                            {
+                                CertificateConfiguration addCertificateConfiguration = new CertificateConfiguration
+                                {
+                                    RootCertificate = certificateExplorerProvider.Certificates[certNumber].Certificate,
+                                    CertName = "ITDepartment",
+                                    OutFolder = @"D:\Users\certificate\",
+                                    Password = "12345678",
+                                };
+                                CertificateGenerationProvider certificateGenerationProvider2 = new CertificateGenerationProvider();
+                                certificateGenerationProvider2.GenerateCertCertificate(addCertificateConfiguration);
+                                Console.WriteLine("Сертификат успешно сгенерирован!");
+                            }
+                            else
+                                Console.WriteLine("Сертификата с таким номером не существует.");
+                            break;
+                        default:
+                            Console.WriteLine("Некорректный номер подпрограммы. Пожалуйста повторите ввод.");
+                            break;
+                    }
+                }
+                else
+                    Console.WriteLine("Некорректный номер подпрограммы. Пожалуйста повторите ввод."); 
+            }
         }
     }
 }
---
+++ SertGenerate/X509Certificate2Wrapper.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SertGenerate
+{
+    public class X509Certificate2Wrapper
+    {
+        private X509Certificate2 cert = null;
+        private string group = null;
+        private string certGroupName = null;
+
+        public X509Certificate2Wrapper(X509Certificate2 cert, string group, string certGroupName)
+        {
+            this.cert = cert;
+            this.group = group;
+            this.certGroupName = certGroupName;
+        }
+
+        public X509Certificate2 Certificate { get { return cert; } }
+
+        public string PublishedFor
+        {
+            get { return cert.GetNameInfo(X509NameType.SimpleName, false); } 
+        }
+    
+        public string Published
+        {
+            get { return cert.GetNameInfo(X509NameType.SimpleName, true); }
+        }
+
+        public string ExpirationDate
+        {
+            get { return cert.GetExpirationDateString(); }
+        }
+
+        public string Group
+        {
+            get { return group; }
+        }
+
+        public string CertGroupName
+        {
+            get { return certGroupName; }
+        }
+
+        public override string ToString()
+        {
+            return $"Group: {Group} ({CertGroupName}\nPublishedFor: {PublishedFor}\nExp: {ExpirationDate}\n)";
+        }
+
+    }
+}
